112bca7a82f98d331beda2abb4168a350666a842
Calypso <calypsotheatre@gmail.com>
2023-12-11 16:07:00 -0500

    Emoji-clone

---
+++ src/app/emojis/[emojiId]/route.js
@@ -0,0 +1,62 @@
+import { emojis } from "@/lib/emojis.js";
+import { NextResponse } from "next/server.js";
+
+export function GET(request, response) {
+  const { emojiId } = response.params;
+  const emoji = emojis.filter((emoji) => emoji.id === +emojiId)[0];
+  if (!emoji) {
+    return NextResponse.json({
+      success: false,
+      message: "No emoji with that ID found.",
+    });
+  }
+  return NextResponse.json({ success: true, emoji });
+}
+
+export async function DELETE(request, response) {
+  try {
+    const { emojiId } = response.params;
+    const index = emojis.findIndex((emoji) => emoji.id === +emojiId);
+
+    if (index === -1) {
+      return NextResponse.json({
+        success: false,
+        message: "No emoji with that ID found.",
+      });
+    }
+    emojis.splice(index, 1);
+    return NextResponse.json({ success: true, emojis });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
+
+export async function PUT(request, response) {
+  try {
+    const { emojiId } = response.params;
+    const index = emojis.findIndex((emoji) => emoji.id === +emojiId);
+
+    if (index === -1) {
+      return NextResponse.json({
+        success: false,
+        message: "No emoji with that ID found.",
+      });
+    }
+
+    const { character, name } = await request.json();
+
+    if (!character || !name) {
+      return NextResponse.json({
+        success: false,
+        message: "You must provide an emoji and name.",
+      });
+    }
+
+    emojis[index].character = character;
+    emojis[index].name = name;
+
+    return NextResponse.json({ success: true, emojis });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
---
+++ src/app/emojis/route.js
@@ -0,0 +1,25 @@
+import { emojis } from "@/lib/emojis.js";
+import { NextResponse } from "next/server.js";
+
+export function GET() {
+  return NextResponse.json({ success: true, emojis });
+}
+
+export async function POST(request, response) {
+  try {
+    const { character, name } = await request.json();
+
+    if (!character || !name) {
+      return NextResponse.json({
+        success: false,
+        error: "You must provide an emoji and name.",
+      });
+    }
+    const emoji = { id: emojis.length + 1, character, name };
+
+    emojis.push(emoji);
+    return NextResponse.json({ success: true, emojis });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
